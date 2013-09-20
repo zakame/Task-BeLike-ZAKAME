b548f532da0eee9d8218501c330179b8b5680448
Zak B. Elep <zakame@zakame.net>
2013-09-20 02:26:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Task::BeLike::ZAKAME - It's new $module
+
+# SYNOPSIS
+
+    use Task::BeLike::ZAKAME;
+
+# DESCRIPTION
+
+Task::BeLike::ZAKAME is ...
+
+# LICENSE
+
+Copyright (C) Zak B. Elep.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Zak B. Elep <zakame@zakame.net>
---
+++ lib/Task/BeLike/ZAKAME.pm
@@ -0,0 +1,39 @@
+package Task::BeLike::ZAKAME;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Task::BeLike::ZAKAME - It's new $module
+
+=head1 SYNOPSIS
+
+    use Task::BeLike::ZAKAME;
+
+=head1 DESCRIPTION
+
+Task::BeLike::ZAKAME is ...
+
+=head1 LICENSE
+
+Copyright (C) Zak B. Elep.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Zak B. Elep E<lt>zakame@zakame.netE<gt>
+
+=cut
+
